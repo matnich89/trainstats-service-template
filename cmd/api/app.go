020f265357ec0e5193b83dcfdd6baa2e3b5cf9ef
mathewnicholls <mathewnicholls@yandex.com>
2024-08-22 20:45:25 +0100

    move method

--- cmd/api/app.go
+++ cmd/api/app.go
@@ -1,8 +1,16 @@
 package cmd
 
 import (
+	"context"
+	"errors"
 	"github.com/go-chi/chi/v5"
 	"github.com/matnich89/trainstats-service-template/handler"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 type App struct {
@@ -19,4 +27,48 @@ func NewApp(router *chi.Mux, handler *handler.Handler) *App {
 
 func (a *App) routes() {
 	a.router.Get("/trains", a.handler.ConnectWS)
+}
+
+func (a *App) Serve() error {
+
+	srv := &http.Server{
+		Addr:         ":8080",
+		Handler:      a.router,
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	shutdownError := make(chan error)
+
+	go func() {
+		quit := make(chan os.Signal, 1)
+
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+		s := <-quit
+
+		log.Printf("caught signal %s", s.String())
+		ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+		defer cancel()
+
+		shutdownError <- srv.Shutdown(ctx)
+
+	}()
+
+	log.Println("starting api...")
+	a.routes()
+	err := srv.ListenAndServe()
+
+	if !errors.Is(err, http.ErrServerClosed) {
+		return err
+	}
+
+	err = <-shutdownError
+	if err != nil {
+		return err
+	}
+	log.Println("stopped api")
+
+	return nil
 }
